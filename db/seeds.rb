38b9854b5f2f6e7ac22c9b341ec3a8bfa59ac0b8
Franco <francorsr98@gmail.com>
2021-04-07 19:32:44 -0500

    Data is generated through the Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Start creating products'
+(1..5).each { Product.create(name: Faker::Commerce.unique.product_name) }
+puts 'End creating products'
+
+puts 'Start creating stores'
+(1..5).each { Store.create(name: Faker::Company.unique.name, priority: rand(1..5)) }
+puts 'End creating stores'
+
+stores = Store.all
+products = Product.all
+
+puts 'Start adding products to the store'
+stores.each do |store|
+  products.first(rand(1..5)).each do |product|
+    store.products << product
+  end
+end
+puts 'End adding products to the store'
+
+puts 'Start creating user by store'
+stores.each do |store|
+  store_email = store.name.split(/-|\s|,/).first.downcase
+  User.create(username: Faker::Lorem.unique.word, email: "#{store_email}@mail.com", 
+              password: '123456', store_name: store.name, store: store)
+end
+puts 'End creating user by store'
