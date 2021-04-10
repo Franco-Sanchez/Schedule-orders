df9f39a1f874816dd609fd2a91b828c356a1ab5a
Franco <francorsr98@gmail.com>
2021-04-10 10:20:48 -0500

    Logic to validate that 22 hours have passed was added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,10 @@ class OrdersController < ApplicationController
 
   # POST /orders
   def create
-    client = Client.new(name: params[:order][:name], email: params[:order][:email],
-                        address: params[:order][:address], phone: params[:order][:phone])
-    if client.save
-      create_order(client)
+    @client = Client.new(name: params[:order][:name], email: params[:order][:email],
+                         address: params[:order][:address], phone: params[:order][:phone])
+    if @client.save
+      create_order
     else
       render :new
     end
@@ -20,20 +20,24 @@ class OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(status: params[:order][:status])
-    params[:order][:price] && @order.update(price: params[:order][:price])
+    update_price
     redirect_to store_path(name: current_user.store.name)
   end
 
   private
 
-  def create_order(client)
+  def create_order
     @order = Order.new(product_name: params[:order][:product_name])
-    @order.client = client
+    @order.client = @client
     @order.store = Order.store_found(params[:order][:product_name])
     if @order.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def update_price
+    params[:order][:price] && @order.update(price: params[:order][:price])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -12,12 +12,18 @@ class Order < ApplicationRecord
     return unless pending_orders.length.positive?
 
     pending_orders.each do |order|
+      next unless confirm_two_hours(order)
+
       order.update(status: 'discarded')
-      unless store_found(order.product_name, order.store).is_a?(Array)
-        create(price: order.price, product_name: order.product_name, client: order.client,
-               store: store_found(order.product_name, order.store))
-      end
+      create_order(order)
     end
+  end
+
+  def self.create_order(order)
+    return if store_found(order.product_name, order.store).is_a?(Array)
+
+    create(price: order.price, product_name: order.product_name, client: order.client,
+           store: store_found(order.product_name, order.store))
   end
 
   def self.store_found(product_name, store_object = nil)
@@ -25,14 +31,14 @@ class Order < ApplicationRecord
     stores_filter = stores.filter do |store|
       store.products.filter { |product| product.name == product_name }.size.positive?
     end
-    if validate_store_object(stores_filter, store_object).length.positive?
-      validate_store_object(stores_filter, store_object).first
+    if saved_stores(stores_filter, store_object).length.positive?
+      saved_stores(stores_filter, store_object).first
     else
-      validate_store_object(stores_filter, store_object)
+      saved_stores(stores_filter, store_object)
     end
   end
 
-  def self.validate_store_object(stores_filter, store_object)
+  def self.saved_stores(stores_filter, store_object)
     if store_object
       stores_sort = stores_filter.sort_by(&:priority)
       store_index = stores_sort.index(store_object)
@@ -41,6 +47,14 @@ class Order < ApplicationRecord
       stores_filter.sort_by(&:priority)
     end
   end
+
+  def self.confirm_two_hours(order)
+    current_time = (DateTime.now.hour * 60) + DateTime.now.min
+    save_time = (order.created_at.hour * 60) + order.created_at.min
+    (current_time - save_time) / 60 == 2
+  end
+
+  # custom validation
 
   def price_approved
     errors.add(:price, 'Price should be greater than 0') if price && price <= 0
